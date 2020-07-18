88409d022b80158c21695b8a42e064443d77ec77
subhamyadav580 <subhamyadav580@gmail.com>
2020-07-19 03:00:21 +0530

    new features

--- chat.js
+++ chat.js
@@ -1,5 +1,6 @@
 // function to search users lists
 let email_id;
+
 function myFunction() {
     var input, filter, ul, li, a, i, txtValue;
     input = document.getElementById("myInput");
@@ -26,6 +27,7 @@ function users() {
     console.log('users() function called');
     document.getElementById('chat_user').style.display = "block";
     document.getElementById('chat').style.display = "block";
+    document.getElementById('myInput').style.display = "block";
     user_email = document.getElementById('inp_mesg').value;
     if (user_email) {
         console.log(user_email);
@@ -41,12 +43,13 @@ function users() {
                 }
                 user_name = sentence.join(" ");
                 var lists = [email_id, user_name]
-                console.log(user_name+"---")
+                console.log(user_name + "---")
                 if (user_email != email_id) {
                     console.log(user_name);
-                    document.querySelector('#myUL').innerHTML += `<li title = ` + user_name + `>
-                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/chat_avatar_01.jpg" alt=""/><div><h2 class = "open-button" id="`+ lists + `" onclick = "openForm(this.id)" style="cursor: pointer;">` + user_name + `</h2><h3><span class  = "status orange"></span>offline</h3></div></li>`;
+                    document.querySelector('#myUL').innerHTML += `<li title = ` + user_name + ` class = "open-button" id="` + lists + `" onclick = "openForm(this.id)" style="cursor: pointer;">
+                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/chat_avatar_01.jpg" alt=""/><div><h2>` + user_name + `</h2><h3><span class  = "status orange"></span>offline</h3></div></li>`;
                     console.log(doc.data().Email)
+                    console.log(lists)
                 }
 
             });
@@ -60,6 +63,7 @@ function users() {
 
 
 let to_user;
+
 function openForm(button_id) {
     var words = button_id.split(",");
     var word1 = words[0];
@@ -104,17 +108,17 @@ function head_user(button_id) {
 
 function message_(button_id) {
     document.getElementById("chat").style.display = "block";
+    document.getElementById('send').style.display = "block";
     console.log(user_email + ":" + button_id)
-    db.collection("messages").orderBy('time').onSnapshot(function (snapshot) {
-        snapshot.docChanges().forEach(function (change) {
+    db.collection("messages").orderBy('time').onSnapshot(function(snapshot) {
+        snapshot.docChanges().forEach(function(change) {
             if (change.type === "added") {
                 if (change.doc.data().from == user_email && change.doc.data().to == button_id) {
                     console.log("true");
                     console.log(user_email + ":" + button_id)
                     document.querySelector('#chat').innerHTML += `<li class="me"><div class="entete"><h3 style="color:black;">` + change.doc.data().time + `</h3><span class="status blue"></span><div class="triangle"></div><div class="message">` + change.doc.data().mesg + `</div></li>`;
                     console.log("message displayed succesfully")
-                }
-                else if (change.doc.data().from == button_id && change.doc.data().to == user_email) {
+                } else if (change.doc.data().from == button_id && change.doc.data().to == user_email) {
                     document.querySelector('#chat').innerHTML += `<li class="you"><div class="entete"><h3 style="color:black;">` + change.doc.data().time + `</h3><span class="status green"></span></div><div class="triangle"></div><div class="message">` + change.doc.data().mesg + `</div></li>`;
                     console.log("message displayed succesfully")
                 }
@@ -133,6 +137,7 @@ function message_(button_id) {
 
 var message;
 var from_user;
+
 function send_messg() {
     console.log(user_email);
     message = document.getElementById('msg').value;
@@ -150,12 +155,71 @@ function send_messg() {
     });
     console.log('message sent!');
     console.log('new_message_sent!');
+    document.getElementById('msg').value = ""
+    document.getElementById('photo').value = ""
+
 
 }
 
 
+// attaching file 
 
+var selectedFile; 
+function getfile() 
+{ 
+    var pic = document.getElementById("photo"); 
 
+    // selected file is that file which user chosen by html form 
+    selectedFile = pic.files[0]; 
 
+    // make save button disabled for few seconds that has id='submit_link' 
+//            document.getElementById('submit_link').setAttribute('disabled', 'true'); 
+    myfunction(); // call below written function 
+} 
+function myfunction() 
+{ 
+    // select unique name for everytime when image uploaded 
+    // Date.now() is function that give current timestamp 
+    var name="123"+Date.now(); 
+
+    // make ref to your firebase storage and select images folder 
+    var storageRef = firebase.storage().ref('/images/'+ name); 
+
+    // put file to firebase 
+    var uploadTask = storageRef.put(selectedFile); 
+
+    // all working for progress bar that in html 
+    // to indicate image uploading... report 
+    uploadTask.on('state_changed', function(snapshot){ 
+        var progress = 
+        (snapshot.bytesTransferred / snapshot.totalBytes) * 100; 
+        var uploader = document.getElementById('uploader'); 
+        uploader.value=progress; 
+        switch (snapshot.state) { 
+            case firebase.storage.TaskState.PAUSED: 
+            console.log('Upload is paused'); 
+            break; 
+            case firebase.storage.TaskState.RUNNING: 
+            console.log('Upload is running'); 
+            break; 
+        } 
+    }, function(error) {console.log(error); 
+    }, function() { 
+
+        // get the uploaded image url back 
+        uploadTask.snapshot.ref.getDownloadURL().then( 
+            function(downloadURL) { 
+
+        // You get your url from here 
+            console.log('File available at', downloadURL); 
+            var text = document.getElementById('msg');
+            text.value = text.value + " "+ downloadURL;
+
+        // print the image url 
+        console.log(downloadURL); 
+        //document.getElementById('submit_link').removeAttribute('disabled'); 
+        }); 
+    }); 
+}; 
 
 
